29be5d93d129b3339ef39ec24a132829647b8c74
yaddamaster <subscriptions@patrickhome.com>
2020-04-25 21:56:35 -0700

    Updates

---
+++ precache-manifest.ae5f2b038d5622ef8e803d12de5edd43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa93c44d0b6087ccdde4f44e01698570",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "54cbf179b2abf370b744",
+    "url": "/monsters-rolodex/static/css/main.86cc6a0c.chunk.css"
+  },
+  {
+    "revision": "0f62713f0e611e05e261",
+    "url": "/monsters-rolodex/static/js/2.3201c238.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.3201c238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54cbf179b2abf370b744",
+    "url": "/monsters-rolodex/static/js/main.c69333ff.chunk.js"
+  },
+  {
+    "revision": "ac7692f361dea471e0d6",
+    "url": "/monsters-rolodex/static/js/runtime-main.ab61569e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
